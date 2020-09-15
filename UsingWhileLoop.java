3b439f993f0a437fb1363dad24572effa6c6e908
prafullakumarsahu <prafullakumarsahu001@gmail.com>
2020-09-16 03:34:51 +0530

    Use while

---
+++ UsingWhileLoop.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package beginerjava;
+
+/**
+ * @author prafullakumarsahu
+ *
+ */
+public class UsingWhileLoop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int i = 0;
+		
+		while (i<10) //when to stop inside operations
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
