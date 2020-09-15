488b0041fbf81fc167f9375581ddfec92ce46086
prafullakumarsahu <prafullakumarsahu001@gmail.com>
2020-09-16 03:25:30 +0530

    Hello world program

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package beginerjava;
+
+/**
+ * @author prafullakumarsahu
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
